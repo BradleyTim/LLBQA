e0d41f9aa40e85322a9985b9a8d5ca3eb77b63a8
BradleyTim <timbradley94@gmail.com>
2020-07-29 16:54:52 +0300

    update settings

--- app/settings.py
+++ app/settings.py
@@ -140,7 +140,7 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 SUMMERNOTE_THEME = 'bs4'
 
 # Heroku: Update database configuration from $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'] = db_from_env
 
 
